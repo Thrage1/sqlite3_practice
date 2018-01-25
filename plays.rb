2f9613636dcab05e7bc0dfb38a2e8d88c70129e9
David Veytsman <dveyts@yahoo.com>
2018-01-25 18:07:07 -0500

    began creating an ORM

---
+++ plays.rb
@@ -0,0 +1,27 @@
+require 'singleton'
+require 'sqlite3'
+
+class PlayDBConnection < SQLite3::Database
+  include Singleton
+  def initialize
+    super('plays.db')
+    self.type_translation = true
+    self.results_as_hash = true
+  end
+
+end
+
+class Play
+  def self.all
+  end
+
+  def initialize(options)
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+end

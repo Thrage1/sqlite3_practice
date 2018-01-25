71d47a679bc91823a29847216ee16eec6b6130c0
David Veytsman <dveyts@yahoo.com>
2018-01-25 14:21:14 -0500

    added the dml language for 2 tables

--- import_db.sql
+++ import_db.sql
@@ -1,13 +1,29 @@
-CREATE TABLE plays(
-id INTEGER PRIMARY KEY,
-title TEXT NOT NULL,
-year INTEGER NOT NULL,
-playwright_id INTEGER, NOT NULL,
-FOREIGN KEY (playwright_id) REFERENCES playwrights(id)
+DROP TABLE plays;
+DROP TABLE playwrights;
+
+
+CREATE TABLE plays (
+  id INTEGER PRIMARY KEY,
+  title TEXT NOT NULL,
+  year INTEGER NOT NULL,
+  playwright_id INTEGER NOT NULL,
+  FOREIGN KEY (playwright_id) REFERENCES playwrights(id)
 );
 
-CREATE TABLE playwrights(
-id INTEGER PRIMARY KEY,
-name TEXT NOT NULL,
-birth_year INTEGER,
+CREATE TABLE playwrights (
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  birth_year INTEGER
 );
+
+INSERT INTO
+  playwrights(name, birth_year)
+VALUES
+  ('Franz Kafka', 1955),
+  ('Terry Goodkind', 1967);
+
+INSERT INTO
+  plays(title, year, playwright_id)
+VALUES
+  ('Wizard''s First Rule', 1999, (select id from playwrights where name = 'Terry Goodkind')),
+  ('The Cockaroach', 1966, (select id from playwrights where name = 'Franz Kafka'));
